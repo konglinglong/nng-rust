2cb97b32c1ec296b6061cf7601c1f7cd9ac5c055
jeikabu <jeikabu@users.noreply.github.com>
2019-10-11 14:57:44 +0300

    Set up CI with Azure Pipelines
    - Working CI/CD via Azure Pipelines

--- build.ps1
+++ build.ps1
@@ -1,9 +1,12 @@
 # Non-terminating errors fail the script immediately
 $ErrorActionPreference = "Stop"
 
-if ($env:platform -eq "x86") {
-    cargo test --features cmake-vs2017
-} else {
+if ($IsWindows) {
+    cargo test
+} elseif ($IsMacOS) {
+    $env:PATH += [IO.Path]::PathSeparator + "$env:HOME/.cargo/bin"
     cargo test --features build-bindgen
     cargo build --features source-update-bindings
+} else {
+    cargo test
 }
